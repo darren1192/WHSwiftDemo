158edbbe24f3fda20de2bebd5078acd39b5fe59f
darren1192 <1192116374@qq.com>
2018-06-25 15:05:36 +0800

    test

---
+++ MPMediaQueryDemo/MPMediaQueryDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MPMediaQueryDemo
+//
+//  Created by share2glory on 2018/6/25.
+//  Copyright © 2018年 WH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
