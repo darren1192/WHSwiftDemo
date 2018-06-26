6397aa483c7cd2ed819335360ef138154115b3a5
darren1192 <1192116374@qq.com>
2018-06-26 13:20:43 +0800

    swift4.1 抖动效果

--- ShakeDemo/ShakeDemo/ViewController.swift
+++ ShakeDemo/ShakeDemo/ViewController.swift
@@ -29,8 +29,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
         self.view.backgroundColor = UIColor.white
         
         self.view.addSubview(shakeHorizontalBtn)
