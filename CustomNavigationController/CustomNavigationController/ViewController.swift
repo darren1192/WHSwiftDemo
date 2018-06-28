14918e6852d30de5d5f86f102cd5ded65371967a
darren1192 <1192116374@qq.com>
2018-06-28 17:22:04 +0800

    暂无内容

--- CustomNavigationController/CustomNavigationController/ViewController.swift
+++ CustomNavigationController/CustomNavigationController/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.red
+        self.view.backgroundColor = UIColor.white
         
     }
 
