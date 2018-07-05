4dbae4e318787fc79680c625749c977a6228b06b
darren1192 <1192116374@qq.com>
2018-07-05 15:04:16 +0800

    swift 4.1 animation

--- Animation/Animation/AnimationGroupViewController.swift
+++ Animation/Animation/AnimationGroupViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class AnimationGroupViewController: UIViewController {
-
+    private var animationView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "CAAnimationGroup"
+        self.view.backgroundColor = UIColor.white
+        
+        animationView = UIView.init(frame: CGRect.init(x: 150, y: 200, width: 80, height: 80))
+        animationView.backgroundColor = UIColor.red
+        self.view.addSubview(animationView)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.5) {
+            let transition = CABasicAnimation.init(keyPath: "transform.translation.x")
+            transition.toValue = 300
+            
+            let rotation = CABasicAnimation.init(keyPath: "transform.rotation")
+            rotation.toValue = CGFloat.pi
+            
+            let scale = CABasicAnimation.init(keyPath: "transform.scale")
+            scale.toValue = 0.1
+            
+            let group = CAAnimationGroup()
+            group.duration = 2
+            group.animations = [transition, rotation, scale]
+            group.isRemovedOnCompletion = false
+            group.fillMode = kCAFillModeForwards
+            
+            self.animationView.layer.add(group, forKey: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- Animation/Animation/TransitionViewController.swift
+++ Animation/Animation/TransitionViewController.swift
@@ -8,29 +8,71 @@
 
 import UIKit
 
-class TransitionViewController: UIViewController {
-
+class TransitionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var animationView: UIView = {
+        let view = UIView.init(frame: CGRect.init(x: 250, y: 200, width: 100, height: 100))
+        view.backgroundColor = UIColor.red
+        return view
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView.init(frame: CGRect.init(x: 20, y: 64, width: 300, height: self.view.bounds.height-64))
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    let titleArray = ["cube","moveIn","reveal","fade","pageCurl","pageUnCurl","suckEffect","rippleEffect","oglFlip","rotate","push","cameraIrisHollowOpen","cameraIrisHollowClose"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "CATransition"
+        self.view.backgroundColor = UIColor.white
+        
+        
+        self.view.addSubview(tableView)
+        self.view.addSubview(animationView)
+        
+        
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = titleArray[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if let type = cell?.textLabel?.text {
+            let animation = CATransition()
+            animation.duration = 0.6
+            animation.fillMode = kCAFillModeForwards
+            animation.timingFunction = CAMediaTimingFunction.init(name: kCAMediaTimingFunctionEaseInEaseOut)
+            animation.type = type
+            if type != "rotate"{
+                animation.subtype = kCATransitionFromLeft
+            }else {
+                animation.subtype = "90cw"
+            }
+            self.animationView.layer.add(animation, forKey: nil)
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
